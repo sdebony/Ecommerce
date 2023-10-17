b0ebd7ad22ef3bc6a9fab206e0bbdee14b255394
sdebony <santidebony@hotmail.com>
2023-10-17 12:50:16 -0300

    Lifche-New-Branch-17/10/2023

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -150,13 +150,10 @@ STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
     'ecomm/static',
 ]
-#ORIGINAL ******
-#MEDIA_URL = '/media/'
-#MEDIA_ROOT = BASE_DIR /'media'
-#ORIGINAL ******
 
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /''
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
