db32016cf2aa4f3016c30b804cc06e11053a1619
sdebony <santidebony@hotmail.com>
2024-04-15 11:24:31 -0300

    Meli2

--- meli/urls.py
+++ meli/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
  
   path('config/', views.meli_list, name='meli_list'),
   path('config/refresh/', views.meli_solicitar_refresh_token, name='meli_solicitar_token'),
-  path('config/save_data/', views.meli_save_token, name='meli_save_token'),
+  path('', views.meli_save_token, name='meli_save_token'),  # Redireciona para salvar o token obtido na url de autenticação do ML  localhost:8000/meli 
   path('config/get_token/', views.meli_get_first_token, name='meli_get_first_token'),
   path('meli/vendor/', views.meli_productos_vendor_detail, name='meli_productos_vendor_detail'),
 
